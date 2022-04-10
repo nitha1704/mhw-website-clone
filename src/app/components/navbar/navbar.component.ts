1ee7625f984a83115dc2ea5c4970cc5bb0c27490
Nitha <nitha.spst@gmail.com>
2022-04-11 02:06:40 +0700

    add navbar-menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  styleUrls: ['./navbar.component.sass'],
 })
 export class NavbarComponent implements OnInit {
+  timer: any;
+  offsetY: number = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  menuActive(navbar: any) {
+    clearTimeout(this.timer);
+
+    const body: any = document.body;
+    const allMenu = document.querySelectorAll('.navbar-menu .content li');
+    navbar.classList.toggle('active');
+
+    if (navbar.classList.contains('active')) {
+      this.offsetY = pageYOffset;
+      body.classList.add('fixed');
+      body.style.top = `-${this.offsetY}px`;
+
+      this.timer = setTimeout(() => {
+        allMenu.forEach((item: any, index) => {
+          item.style.animation = `menuFade ${(index + 1.5) * 0.3}s`;
+          item.style.animationFillMode = 'forwards';
+        });
+      }, 800);
+    } else {
+      allMenu.forEach((item: any) => {
+        item.removeAttribute('style');
+      });
+      body.classList.remove('fixed');
+      window.scrollTo(0, this.offsetY);
+    }
   }
-
 }
