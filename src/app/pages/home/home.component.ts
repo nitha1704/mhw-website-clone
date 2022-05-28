30ad5e9369759613de8ade2ee1eff242a4fd8c12
Nitha <nitha.spst@gmail.com>
2022-05-29 01:29:07 +0700

    add background video to ecosystem and hunting section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -14,12 +14,24 @@ export class HomeComponent implements OnInit {
   }
 
   startAnimation() {
-    const bgVideo: any = document.querySelector('.bg-video');
-    const introVideo: any = document.querySelector('.bg-video video');
+    const homeBgVideo: any = document.querySelector('#home .bg-video');
+    const homeIntroVideo: any = document.querySelector('#home .bg-video video');
     setTimeout(() => {
-      bgVideo.classList.add('show');
-      introVideo.play();
+      homeBgVideo.classList.add('show');
+      homeIntroVideo.play();
     }, 3000);
+
+    // const allBgVideo: any = document.querySelectorAll('.bg-video');
+    // const allIntroVideo: any = document.querySelectorAll('.bg-video video');
+    // setTimeout(() => {
+    //   allBgVideo.forEach((bgVideo: any) => {
+    //     bgVideo.classList.add('show');
+    //   });
+
+    //   allIntroVideo.forEach((introVideo: any) => {
+    //     introVideo.play();
+    //   });
+    // }, 3000);
   }
 
   isInViewPort(elem: any, type: any = 'normal') {
@@ -29,9 +41,15 @@ export class HomeComponent implements OnInit {
     let scrollBottom = window.pageYOffset + window.innerHeight;
 
     if (type === 'section') {
-      return scrollBottom > elemTop + 500;
+      return scrollBottom > elemBottom - 30;
     }
-    return scrollBottom > elemTop + 200;
+    if (type === 'paragraph') {
+      return scrollBottom > elemTop + 200;
+    }
+    if (type === 'mhw-button') {
+      return scrollBottom > elemTop + 50;
+    }
+    return scrollBottom > elemTop;
   }
 
   typeWriter(type: string, messageToShow: any, elem: any, currentPos: any = 0) {
@@ -80,30 +98,63 @@ export class HomeComponent implements OnInit {
     let paragraphElement = document.querySelectorAll(
       '.section-element .paragraph'
     );
+    let mhwButton2Element = document.querySelectorAll(
+      '.section-element .mhw-button2'
+    );
 
-    // Section add relative position
+    // Ecosystem
+    let ecosystemSection: any = document.querySelector('#ecosystem');
+    let ecosystemBgVideo: any = document.querySelector('#ecosystem .bg-video');
+    let ecosystemIntroVideo: any = document.querySelector(
+      '#ecosystem .bg-video video'
+    );
+
+    // Hunting
+    let huntingSection: any = document.querySelector('#hunting');
+    let huntingBgVideo: any = document.querySelector('#hunting .bg-video');
+    let huntingIntroVideo: any = document.querySelector(
+      '#hunting .bg-video video'
+    );
+
     window.addEventListener('scroll', () => {
-      sectionElement.forEach((elem: any) => {
-        if (
-          this.isInViewPort(elem, 'section') &&
-          !elem.classList.contains('in-view')
-        ) {
-          elem.style.position = 'relative';
+      // Play Ecosystem video background
+      if (
+        this.isInViewPort(ecosystemSection, 'section') &&
+        !ecosystemSection.classList.contains('in-view')
+      ) {
+        setTimeout(() => {
+          ecosystemSection.style.position = 'relative';
+          ecosystemBgVideo.classList.add('show');
+          ecosystemIntroVideo.play();
+        }, 1000);
+      }
+
+      // Play Hunting video background
+      if (
+        this.isInViewPort(huntingSection, 'section') &&
+        !huntingSection.classList.contains('in-view')
+      ) {
+        setTimeout(() => {
+          huntingSection.style.position = 'relative';
+          huntingBgVideo.classList.add('show');
+          huntingIntroVideo.play();
+        }, 1000);
+      }
+
+      //Title Fade Effect
+      titleElement.forEach((elem: any) => {
+        if (this.isInViewPort(elem) && !elem.classList.contains('in-view')) {
+          elem.classList.add('fadeInLeftTitle');
         }
       });
-
-      // Title Fade Effect
-      // titleElement.forEach((elem: any) => {
-      //   let textData: any = elem.getAttribute('text-data');
-      //   if (this.isInViewPort(elem) && !elem.classList.contains('in-view')) {
-      //     this.typeWriter('normal', textData, elem);
-      //   }
-      // });
 
       // Subtitle Typewriter Effect
       subtitleElement.forEach((elem: any) => {
         let textData: any = elem.getAttribute('text-data');
-        if (this.isInViewPort(elem) && !elem.classList.contains('in-view')) {
+        if (
+          this.isInViewPort(elem, 'paragraph') &&
+          !elem.classList.contains('in-view')
+        ) {
           this.typeWriter('reverse', textData, elem);
         }
       });
@@ -111,8 +162,22 @@ export class HomeComponent implements OnInit {
       // Paragraph Typewriter Effect
       paragraphElement.forEach((elem: any) => {
         let textData: any = elem.getAttribute('text-data');
-        if (this.isInViewPort(elem) && !elem.classList.contains('in-view')) {
+        if (
+          this.isInViewPort(elem, 'paragraph') &&
+          !elem.classList.contains('in-view')
+        ) {
           this.typeWriter('normal', textData, elem);
+        }
+      });
+
+      // mhwButton2  Typewriter Effect
+      mhwButton2Element.forEach((elem: any) => {
+        if (
+          this.isInViewPort(elem, 'mhw-button') &&
+          !elem.classList.contains('in-view')
+        ) {
+          elem.classList.add('in-view');
+          elem.style.animation = 'fadeIn 1s forwards';
         }
       });
     });
